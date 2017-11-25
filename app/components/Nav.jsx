fc0f8426f286e7b30c25e11aea575e4a4d6425fe
vern01 <verdunw2@gmail.com>
2017-11-25 21:33:37 +0200

    Fixed modal and more

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
     onSearch: function (e) {
         e.preventDefault();
-        alert("Not wired up yet.");
+
+        var location = this.refs.location.value;
+        location = encodeURIComponent(location);
+
+        if (location.length > 0) {
+            this.refs.location.value = '';
+            window.location.hash = '#/?location=' + location;
+        }
     },
     render: function () {
         return (
@@ -20,7 +27,7 @@ var Nav = React.createClass({
                 <div className="top-bar-right">
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
-                            <li><input type="search" placeholder="Search Weather"/></li>
+                            <li><input type="search" placeholder="Search Weather" ref="location"/></li>
                             <li><input type="submit" className="button" value="Get Weather"/></li>
                         </ul>
                     </form>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,18 +2,22 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
     getInitialState: function () {
         return {
-            isLoading: false
+            isLoading: false,
         };
     },
     handleSearch: function (location) {
         var that = this;
 
         this.setState({
-            isLoading: true
+            isLoading: true,
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(function (temp) {
@@ -22,15 +26,31 @@ var Weather = React.createClass({
                 temp: temp,
                 isLoading: false
             });
-        }, function (errorMessage) {
+        }, function (e) {
             that.setState({
-                isLoading: false
+                isLoading: false,
+                errorMessage: e.message
             });
-            alert(errorMessage);
         });
     },
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+    componentWillReceiveProps: function (newProps) {
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
     render: function () {
-        var {isLoading, location, temp} = this.state;
+        var {isLoading, location, temp, errorMessage} = this.state;
 
         function renderMessage() {
             if (isLoading) {
@@ -40,11 +60,20 @@ var Weather = React.createClass({
             }
         }
 
+        function renderError() {
+            if (typeof errorMessage === 'string') {
+                return (
+                    <ErrorModal message={errorMessage}/>
+                );
+            }
+        }
+
         return (
             <div>
-                <h1 className="text-center">Get Weather</h1>
+                <h1 className="text-center page-title">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
