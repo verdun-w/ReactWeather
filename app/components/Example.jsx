97908ef92980b44404ef5a18cbacabf2e349a5ab
vern01 <verdunw2@gmail.com>
2017-11-25 14:41:01 +0200

    Styling for examples and main base

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,12 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => {
     return(
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to Examples</p>
+            <h1 classname="text-center">Examples</h1>
+            <p>Here are a few examples location to try out:</p>
+            <ol>
+                <li><Link to="/?location=George">George</Link></li>
+                <li><Link to="/?location=Rio">Rio</Link></li>
+            </ol>
         </div>
-
     );
 };
 
